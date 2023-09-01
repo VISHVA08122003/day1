3b7cdbdc4cf6689332bb3b9ce2dad6ebb073f48a
VISHVA08122003 <108726060+VISHVA08122003@users.noreply.github.com>
2023-09-01 16:05:33 +0530

    Add files via upload

---
+++ src/main/java/com/example/spring/day8/service/BikeService.java
@@ -0,0 +1,29 @@
+package com.example.spring.day8.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.spring.day8.model.BikeModel;
+import com.example.spring.day8.repository.BikeRepo;
+
+@Service
+public class BikeService {
+	
+	@Autowired
+	BikeRepo br;
+	public BikeModel add(BikeModel s) {
+		return br.save(s);
+	}
+	public List<BikeModel> getByYear(int year){
+		return br.getByYear(year);
+	}
+	public List<BikeModel> getByYearAndName(int year,String name){
+		return br.getByYearAndName(year, name);
+	}
+	public List<BikeModel> getByName(String name){
+		return br.getByName(name);
+	}
+
+}
